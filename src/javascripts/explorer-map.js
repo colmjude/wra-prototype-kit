0b91198d842d07ecf0540ef9d208eb80c545db90
Colm Britton <colmjude@gmail.com>
2022-06-24 09:31:54 +0100

    Explorer map makes use of map module

--- src/javascripts/explorer-map.js
+++ src/javascripts/explorer-map.js
@@ -1,5 +1,6 @@
 /* global maplibregl, fetch */
 import mapHelpers from '../javascripts/map-helpers'
+import * as WRA from './map-prototypes'
 
 // helpers used in script
 const createMarker = mapHelpers.createMarker
@@ -9,105 +10,31 @@ const LA_BOUNDARIES_ENDPOINT = 'https://geoserverlp.azurewebsites.net/geoserver/
 //const LA_BOUNDARIES_ENDPOINT = '/static/data/platform-la-boundaries.json'
 const attrToMatchOn = 'name_en'
 
-const createMap = function () {
-  const map = new maplibregl.Map({
-    container: 'mapId', // container id
-    style: '/static/javascripts/base-tile.json',
-    center: [-3.7, 52.4], // starting position [lng, lat]
-    zoom: 6.89// starting zoom
-  })
-
-  map.addControl(new maplibregl.FullscreenControl({
-    container: document.querySelector('#mapId')
-  }), 'bottom-left')
-  return map
-}
-
-function renderBoundaries (map) {
+function renderBoundaries (appmap) {
   fetch(LA_BOUNDARIES_ENDPOINT)
     .then(response => response.json())
     .then(function (data) {
       console.log(data)
-      map.addSource('laBoundaries', {
-        type: 'geojson',
-        data: LA_BOUNDARIES_ENDPOINT
+      appmap.addGeojsonSource('laBoundaries', LA_BOUNDARIES_ENDPOINT)
+      appmap.addPolygonLayer('laLayer', 'laBoundaries', {
+        fillColor: '#088',
+        fillOpacity: 0.4,
+        lineColor: '#088',
+        lineOpacity: 0.8,
+        lineWidth: 1
       })
-      map.addLayer({
-        id: 'laLayer',
-        type: 'fill',
-        source: 'laBoundaries',
-        layout: {},
-        paint: {
-          'fill-color': '#088',
-          'fill-opacity': 0.4
-        }
-      })
-      map.addLayer({
-        id: 'laLayerLine',
-        type: 'line',
-        source: 'laBoundaries',
-        layout: {},
-        paint: {
-          'line-color': '#088',
-          'line-opacity': 0.8,
-          'line-width': 1
-        }
-      })
-
-      map.setPaintProperty('laLayer', 'fill-opacity', [
-        'interpolate',
-        // Set the exponential rate of change to 0.5
-        ['exponential', 0.5],
-        ['zoom'],
-        // When zoom is 15, buildings will be beige.
-        6,
-        0.6,
-        // When zoom is 18 or higher, buildings will be yellow.
-        15,
-        0.2
-      ])
-
-      map.setPaintProperty('laLayerLine', 'line-width', [
-        'interpolate',
-        // Set the exponential rate of change to 0.5
-        ['exponential', 0.5],
-        ['zoom'],
-        // When zoom is 15, buildings will be beige.
-        7,
-        1,
-        // When zoom is 18 or higher, buildings will be yellow.
-        15,
-        3
-      ])
     })
 }
 
 const nationalParkEndpoint = 'https://geoserverlp.azurewebsites.net/geoserver/test/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=test%3ANRW_NATIONAL_PARKPolygon4326&maxFeatures=50&outputFormat=application%2Fjson'
-function renderNationalParks (map) {
-  map.addSource('nationalParks', {
-    type: 'geojson',
-    data: nationalParkEndpoint
-  })
-  map.addLayer({
-    id: 'nationalParkLayer',
-    type: 'fill',
-    source: 'nationalParks',
-    layout: {},
-    paint: {
-      'fill-color': '#800',
-      'fill-opacity': 0.4
-    }
-  })
-  map.addLayer({
-    id: 'nationalParkLayerLine',
-    type: 'line',
-    source: 'nationalParks',
-    layout: {},
-    paint: {
-      'line-color': '#800',
-      'line-opacity': 0.8,
-      'line-width': 1
-    }
+function renderNationalParks (appmap) {
+  appmap.addGeojsonSource('nationalParks', nationalParkEndpoint)
+  appmap.addPolygonLayer('nationalPark', 'nationalParks', {
+    fillColor: '#800',
+    fillOpacity: 0.4,
+    lineColor: '#800',
+    lineOpacity: 0.8,
+    lineWidth: 1
   })
 }
 
@@ -116,23 +43,27 @@ const getLocalAuthorityFeatures = function (map, pt) {
   return features.filter(feature => feature.layer.source === 'laBoundaries')
 }
 
-const map = createMap()
 const marker = createMarker()
 
-// add listener for when initial map loaded
-map.on('load', function (e) {
+function loadHandler (module) {
   console.log('map loaded')
-  renderBoundaries(map)
-  renderNationalParks(map)
+  renderBoundaries(module)
+  renderNationalParks(module)
+}
+
+const $mapEl = document.querySelector('[data-module="wra-map"]')
+const mapComponent = new WRA.Map($mapEl).init({
+  onLoadCallback: loadHandler
 })
+
 // add listener for when map clicked on
-map.on('click', function (e) {
+mapComponent.addEventHandler('click', function (e, appmap) {
+  const map = appmap.getMap()
   console.log('map clicked on', e)
   console.log('click location', e.lngLat.lng, e.lngLat.lat)
   console.log('point', e.point)
   const boundaries = getLocalAuthorityFeatures(map, e.point)
   console.log('clicked on boundaries', boundaries)
-
 
   // put marker at point user clicked
   marker
