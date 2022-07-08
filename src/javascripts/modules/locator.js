ea114ded69183107f8b8e326f22288f178beb946
Colm Britton <colmjude@gmail.com>
2022-07-08 13:01:56 +0100

    Reorganise pinpoint js code

---
+++ src/javascripts/modules/locator.js
@@ -0,0 +1,95 @@
+import utils from '../utils.js'
+import geoHelpers from '../geohelpers'
+
+/* global fetch, CustomEvent */
+
+function Locator ($inputContainer, $summaryContainer) {
+  this.$inputContainer = $inputContainer
+  this.$summaryContainer = $summaryContainer
+}
+
+Locator.prototype.init = function (options) {
+  this.setOptions(options)
+
+  this.$locationDisplay = this.$summaryContainer.querySelector('.app-current-location')
+  this.$locationDisplay.classList.add('js-hidden')
+
+  this.$resultsContainer = this.$summaryContainer.querySelector('.app-locator-results')
+  this.$resultsContainer.classList.add('js-hidden')
+
+  // listen for click on current location option
+  this.$curentLocationBtn = this.$inputContainer.querySelector('.app-current-location-button')
+  const boundGetCurrentLocation = this.getCurrentLocation.bind(this)
+  this.$curentLocationBtn.addEventListener('click', boundGetCurrentLocation)
+
+  // listen for clicks on defautl option
+  this.$defaultLocationBtn = this.$inputContainer.querySelector('.app-default-location-button')
+  const boundUseDefaultCoords = this.useDefaultCoords.bind(this)
+  this.$defaultLocationBtn.addEventListener('click', boundUseDefaultCoords)
+}
+
+Locator.prototype.dispatchDataEvent = function (data) {
+  const dataEvent = new CustomEvent('dataRetrieved', { detail: { data: data } })
+  this.$summaryContainer.dispatchEvent(dataEvent)
+}
+
+Locator.prototype.displayLocation = function (coords) {
+  this.$locationDisplay.classList.remove('js-hidden')
+  const $lat = this.$locationDisplay.querySelector('.app-current-location--latitude')
+  const $lng = this.$locationDisplay.querySelector('.app-current-location--longitude')
+  $lat.textContent = coords.latitude
+  $lng.textContent = coords.longitude
+}
+
+Locator.prototype.getBBoxFromPoint = function (coords) {
+  return geoHelpers.getBBox(geoHelpers.convertPointToFeature(coords.latitude, coords.longitude, 0.01))
+}
+
+Locator.prototype.getCurrentLocation = function () {
+  const that = this
+  geoHelpers.getLocation(function (e) {
+    that.displayLocation(e.coords)
+    that.performQuery(e.coords)
+  }, function (e) {
+    console.log('Error', e)
+  })
+}
+
+Locator.prototype.performQuery = function (coords) {
+  const that = this
+  const bbox = this.getBBoxFromPoint(coords)
+  const endpoint = `https://landplatform.azurefd.net/geoserver/test/wms?service=WMS&version=1.1.0&request=GetMap&layers=${this.options.layersStr}&bbox=${bbox[1]}%2C${bbox[0]}%2C${bbox[3]}%2C${bbox[2]}&width=768&height=523&srs=EPSG%3A4326&styles=&format=geojson`
+  console.log('query endpoint', endpoint)
+  // hide results until data collected
+  // worth adding a loader?
+  this.$resultsContainer.classList.add('js-hidden')
+  fetch(endpoint)
+    .then(response => response.json())
+    .then(function (data) {
+      // displayIfInWales(isInWales(data.features))
+      that.$resultsContainer.classList.remove('js-hidden')
+      that.dispatchDataEvent(data)
+      console.log(data)
+    })
+}
+
+Locator.prototype.useDefaultCoords = function () {
+  const coords = {
+    latitude: 52.05935795369581,
+    longitude: -3.5725398780735134
+  }
+  this.displayLocation(coords)
+  this.performQuery(coords)
+}
+
+Locator.prototype.setOptions = function (opts) {
+  const options = opts || {}
+  this.options = utils.extend(locatorDefaults, options)
+}
+
+const locatorDefaults = {
+  elementSelector: '.app-dynamic__osm-address',
+  layersStr: 'HighWaterMark4326,NRW_NATIONAL_PARKPolygon4326,GWC21_Community_Wards4326,conservation_areas,nationalBoundaries'
+}
+
+export default Locator
--- src/javascripts/pinpoint.js
+++ src/javascripts/pinpoint.js
@@ -1,56 +1,28 @@
 /* global fetch */
 
 import geoHelpers from './geohelpers'
+import Locator from './modules/locator'
 
-const $currentLocationDisplay = document.querySelector('.app-current-location')
-$currentLocationDisplay.classList.add('js-hidden')
-
-function getBBoxFromPoint (coords) {
-  return geoHelpers.getBBox(geoHelpers.convertPointToFeature(coords.latitude, coords.longitude, 0.01))
-}
-
-function getCurrentLocation () {
-  geoHelpers.getLocation(function (e) {
-    console.log('Success', e)
-    displayLocation(e.coords)
-    const bbox = getBBoxFromPoint(e.coords)
-    console.log('Current location', bbox)
-    fetchData(bbox)
-  }, function (e) {
-    console.log('Error', e)
-  })
-}
-
-function useDefaultCoords () {
-  const coords = {
-    latitude: 52.05935795369581,
-    longitude: -3.5725398780735134
+function displayIfInWales (feature) {
+  console.log('displaying statement')
+  const $walesStatement = document.querySelector('[data-dynamic-element="in-wales-check"]')
+  $walesStatement.classList.remove('js-hidden')
+  let statement = 'Location NOT in Wales'
+  if (feature.length) {
+    statement = 'Location in Wales'
   }
-  displayLocation(coords)
-  const bbox = getBBoxFromPoint(coords)
-  fetchData(bbox)
+  $walesStatement.textContent = statement
 }
 
-function displayLocation (coords) {
-  $currentLocationDisplay.classList.remove('js-hidden')
-  const $lat = $currentLocationDisplay.querySelector('.app-current-location--latitude')
-  const $lng = $currentLocationDisplay.querySelector('.app-current-location--longitude')
-  $lat.textContent = coords.latitude
-  $lng.textContent = coords.longitude
-}
+const $inputContainer = document.querySelector('[data-locator="locator-inputs"]')
+const $summaryContainer = document.querySelector('[data-locator="locator-summary"]')
+const locator = new Locator($inputContainer, $summaryContainer).init({})
+// $currentLocationDisplay.classList.add('js-hidden')
 
-function fetchData (bbox, successCallback, errorCallback) {
-  // need to make sure all layers are listed
-  // https://landplatform.azurefd.net/geoserver/test/wms?service=WMS&version=1.1.0&request=GetMap&layers=HighWaterMark4326,NRW_NATIONAL_PARKPolygon4326,GWC21_Community_Wards4326,conservation_areas&bbox=-4.057148676659978%2C51.90456243131746%2C-2.923089621907593%2C52.3365572489885&width=768&height=523&srs=EPSG%3A4326&styles=&format=geojson
-  const endpoint = `https://landplatform.azurefd.net/geoserver/test/wms?service=WMS&version=1.1.0&request=GetMap&layers=HighWaterMark4326,NRW_NATIONAL_PARKPolygon4326,GWC21_Community_Wards4326,conservation_areas,nationalBoundaries&bbox=${bbox[1]}%2C${bbox[0]}%2C${bbox[3]}%2C${bbox[2]}&width=768&height=523&srs=EPSG%3A4326&styles=&format=geojson`
-  console.log('query endpoint', endpoint)
-  fetch(endpoint)
-    .then(response => response.json())
-    .then(function (data) {
-      displayIfInWales(isInWales(data.features))
-      console.log(data)
-    })
-}
+$summaryContainer.addEventListener('dataRetrieved', function (e) {
+  console.log('Event heard', e)
+  displayIfInWales(isInWales(e.detail.data.features))
+})
 
 function isInWales (features) {
   const statisticalGeographyField = 'ctry18cd'
@@ -65,24 +37,3 @@ function isInWales (features) {
     return false
   })
 }
-
-function displayIfInWales (feature) {
-  const $walesStatement = document.querySelector('[data-dynamic-element="in-wales-check"]')
-  $walesStatement.classList.remove('js-hidden')
-  let statement = 'Location NOT in Wales'
-  if (feature.length) {
-    statement = 'Location in Wales'
-  }
-  $walesStatement.textContent = statement
-}
-
-const $curentLocationBtn = document.querySelector('.app-current-location-button')
-$curentLocationBtn.addEventListener('click', function (e) {
-  getCurrentLocation()
-})
-
-const $defaultLocationBtn = document.querySelector('.app-default-location-button')
-$defaultLocationBtn.addEventListener('click', function (e) {
-  e.preventDefault()
-  useDefaultCoords()
-})
