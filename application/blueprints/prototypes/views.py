28c3525045f1e443d4e3e8cfea07b6d8d38b2c35
Colm Britton <colmjude@gmail.com>
2022-07-14 17:41:45 +0100

    Add routes for eng and welsh versions or pinpoint page

--- application/blueprints/prototypes/views.py
+++ application/blueprints/prototypes/views.py
@@ -1,4 +1,12 @@
-from flask import redirect, render_template, Blueprint, current_app, url_for, request
+from flask import (
+    abort,
+    redirect,
+    render_template,
+    Blueprint,
+    current_app,
+    url_for,
+    request,
+)
 
 
 prototypes = Blueprint("prototypes", __name__, url_prefix="/prototypes")
@@ -6,4 +14,11 @@ prototypes = Blueprint("prototypes", __name__, url_prefix="/prototypes")
 
 @prototypes.route("/pinpoint")
 def pinpoint():
-    return render_template("prototypes/pinpoint.html")
+    return redirect(url_for("prototypes.pinpoint_bi", lang="en"))
+
+
+@prototypes.route("/<lang>/pinpoint")
+def pinpoint_bi(lang):
+    if lang not in ["en", "cy"]:
+        abort(404)
+    return render_template("prototypes/pinpoint.html", pageLang=lang)
