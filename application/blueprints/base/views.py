796c457bdd94bc8850961e460ab2c10e46208388
Colm Britton <colmjude@gmail.com>
2022-07-26 15:13:11 +0100

    Add a welsh version of index page

--- application/blueprints/base/views.py
+++ application/blueprints/base/views.py
@@ -1,6 +1,7 @@
 import json
 
-from flask import render_template, Blueprint, current_app
+from flask import render_template, Blueprint, current_app, g, abort, url_for, redirect
+from flask_babel import refresh
 
 
 base = Blueprint("base", __name__)
@@ -18,4 +19,14 @@ def read_json_file(data_file_path):
 @base.route("/")
 @base.route("/index")
 def index():
-    return render_template("index.html")
+    return redirect(url_for("base.index_bi", lang="en"))
+
+
+@base.route("/<lang>/")
+@base.route("/<lang>/index")
+def index_bi(lang):
+    if lang.lower() not in ["en", "cy"]:
+        abort(404)
+    g.lang_code = lang
+    refresh()
+    return render_template("index.html", pageLang=lang.lower())
