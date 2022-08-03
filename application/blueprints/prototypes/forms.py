92eba70c3a65e1fee137f971ca9eb6c7aa204b10
Colm Britton <colmjude@gmail.com>
2022-08-03 09:30:59 +0100

    Add welsh translations for by postcode prototype

--- application/blueprints/prototypes/forms.py
+++ application/blueprints/prototypes/forms.py
@@ -2,9 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import SelectField
 from wtforms.validators import DataRequired
 
+from flask_babel import lazy_gettext
+
 
 class PostCodeForm(FlaskForm):
     new_postcode = SelectField(
-        "Select a postcode",
+        lazy_gettext("Select a postcode"),
         validators=[DataRequired(message="You need to select one or more postcodes")],
     )
